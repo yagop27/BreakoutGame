c47d566f171e7a70b512f08cb9c0a87f15b68f25
yagop27 <yago.pais@gmail.com>
2024-10-01 23:17:57 -0300

    Breakout game using turtle library

--- bricks.py
+++ bricks.py
@@ -2,10 +2,11 @@ from turtle import Turtle
 
 
 class Bricks(Turtle):
-    def __init__(self, position):
+    def __init__(self):
         super().__init__()
         self.shape("square")
         self.penup()
         self.color("white")
         self.shapesize(stretch_len=2, stretch_wid=0.7)
-        self.goto(position)
+        self.speed('fastest')
+
--- main.py
+++ main.py
@@ -10,16 +10,18 @@ screen.title("Breakout")
 
 paddle = Paddle()  # Creating the paddle via the class on the paddle file
 ball = Ball()  # Creating the ball via the class on the ball file
-
+bricks = []
 # Setting up the screen to recognize the keys to move the paddle in game
 screen.listen()
 screen.onkey(paddle.move_left, "Left")
 screen.onkey(paddle.move_right, "Right")
 y_cord = 295
-for y in range(7):
+for y in range(7):  # Creating the bricks for the game
     y_cord -= 25
     for i in range(16):
-        Bricks((-320 + (i * 43), y_cord))
+        br = Bricks()
+        br.goto((-320 + (i * 43), y_cord))
+        bricks.append(br)
 
 game_on = True
 while game_on:
@@ -32,11 +34,16 @@ while game_on:
     if ball.xcor() > 380 or ball.xcor() < -380:  # Detecting collision with the side walls
         ball.bounce_x()
 
-    if ball.distance(paddle) < 50 and ball.ycor() < -230:
+    if ball.distance(paddle) < 40 and ball.ycor() < -220:  # Detecting collision with the paddle
         ball.bounce_y()
 
-    if ball.ycor() < -260:
+    if ball.ycor() < -280:
         print('You lose')
         break
+
+    for b in bricks:
+        if ball.distance(b) < 40:
+            b.reset()
+            ball.bounce_y()
 
 screen.exitonclick()
